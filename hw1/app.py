eae2a60336fcc30f3fe0a0e9fe9cb8a74a3bf5c1
Greg <fedardostoewscky@yandex.ru>
2025-09-27 14:54:24 +0300

    intermediate

--- hw1/app.py
+++ hw1/app.py
@@ -1,5 +1,7 @@
 from typing import Any, Awaitable, Callable
-
+import json 
+import math
+from urllib.parse import parse_qs
 
 async def application(
     scope: dict[str, Any],
@@ -12,8 +14,66 @@ async def application(
         receive: Корутина для получения сообщений от клиента
         send: Корутина для отправки сообщений клиенту
     """
-    # TODO: Ваша реализация здесь
+    if scope['type'] != 'http':
+        return
+    method = scope['method']
+    path = scope['path']
+    query_string = scope.get('query_string', b'').decode('utf-8')
+    query_params = parse_qs(query_string)
 
+    async def send_response(status_code, body):
+        await send({
+            'type':'http.response.start', 
+            'status': status_code, 
+            'headers': [
+                [b'content-type', b'application/json'], 
+            ],
+        })
+        await send({
+            'type': 'http.response.body', 
+            'body': json.dumps(body).encode('utf-8'),
+        })
+    async def receive_body():
+        body = b''
+        while True:
+            message = await receive()
+            if message['type'] == 'http.request':
+                body += message.get('body', 'b')
+                if not message.get('more_body', False):
+                    break
+        if body:
+            try:
+                return json.loads(body.decode('utf-8'))
+            except json.JSONDecodeError:
+                return None
+        return None
+    if method != 'GET':
+        await send_response(404, {})
+        return
+    
+    try:
+        if path == '/factorial':
+            if 'n' not in query_params or not query_params['n'][0]:
+                await send_response(422, {})
+                return
+            
+            try:
+                n = int(query_params['n'][0])
+                if n < 0:
+                    await send_response(400, {})
+                    return
+                
+                result = math.factorial(n)
+                await send_response(200, {'result': result})
+                return
+            except ValueError:
+              await send_response(422, {})
+              return
+        
+
+    except Exception:
+        await send_response(500, {})
+        return
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app:application", host="0.0.0.0", port=8000, reload=True)
