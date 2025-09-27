b6ed0b177ba166282eb0e679a8e424942ca8a7b6
Dan Sukhorukov <dsuh0i.h8@gmail.com>
2025-09-27 20:26:17 +0300

    Добавлено решение задачи с websocket'ами.
    Также добавлена доп. зависимость для работы в виде websockets==15.0.1 и
    файл с простыми тестами на комнаты (test_websocket_chat.py).

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from shop_api.routers.item import router as item
 from shop_api.routers.cart import router as cart
+from shop_api.routers.chat import router as chat
 
 app = FastAPI(title="Shop API")
 
 app.include_router(item)
 app.include_router(cart)
+
+app.include_router(chat)
 
 if __name__ == "__main__":
     import uvicorn
