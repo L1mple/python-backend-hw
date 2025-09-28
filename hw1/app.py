8ec9a43960436d49a061c8f28bbf71a015d61993
MIKHAIL BONDAREV <mixail.bondarev@gmail.com>
2025-09-28 19:20:47 +0200

    fix

--- hw1/app.py
+++ hw1/app.py
@@ -128,4 +128,4 @@ async def application(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:application", host="0.0.0.0", port=3000, reload=True)
+    uvicorn.run("app:application", host="0.0.0.0", port=8000, reload=True)
