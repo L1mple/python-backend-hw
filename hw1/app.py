f95fc8d4efc817b58af58642558ee6d5e3860c5d
Margarita <kulachenkova@gmail.com>
2025-09-28 19:19:23 +0300

    refactoring

--- hw1/app.py
+++ hw1/app.py
@@ -19,6 +19,18 @@ def fibonacci(n: int):
     return b
 
 
+async def send_resp(send: Callable[[dict[str, Any]], Awaitable[None]], status: int, msg: bytes):
+    await send({
+        'type': 'http.response.start',
+        'status': status,
+        'headers': [[b'content-type', b'application/json']],
+        })
+    await send({
+        'type': 'http.response.body',
+        'body': msg,
+    })
+
+
 async def application(
     scope: dict[str, Any],
     receive: Callable[[], Awaitable[dict[str, Any]]],
@@ -39,39 +51,14 @@ async def application(
                 param_int = int(param)
 
                 if param_int < 0:
-                    await send({
-                        'type': 'http.response.start',
-                        'status': 400,
-                        'headers': [[b'content-type', b'application/json']],
-                        })
-                    await send({
-                        'type': 'http.response.body',
-                        'body': json.dumps({"error": "n must be > 0"}).encode('utf-8'),
-                    })
+                    await send_resp(send, 400, json.dumps({"error": "n must be > 0"}).encode('utf-8'))
                     return
                 
                 res = fibonacci(param_int)
-
-                await send({
-                    'type': 'http.response.start',
-                    'status': 200,
-                    'headers': [[b'content-type', b'application/json']],
-                })
-                await send({
-                    'type': 'http.response.body',
-                    'body': json.dumps({"result": res}).encode('utf-8'),
-                })
+                await send_resp(send, 200, json.dumps({"result": res}).encode('utf-8'))
 
             except:
-                await send({
-                    'type': 'http.response.start',
-                    'status': 422,
-                    'headers': [[b'content-type', b'application/json']],
-                })
-                await send({
-                    'type': 'http.response.body',
-                    'body': json.dumps({"error": "Invalid parameter"}).encode('utf-8'),
-                })
+                await send_resp(send, 422, json.dumps({"error": "Invalid parameter"}).encode('utf-8'))
 
     elif scope.get("path") and "/factorial" in scope.get("path"):
         try:
@@ -84,51 +71,17 @@ async def application(
                 n = int(n_values[0])
 
                 if n < 0:
-                    await send({
-                        'type': 'http.response.start',
-                        'status': 400,
-                        'headers': [[b'content-type', b'application/json']],
-                        })
-                    await send({
-                        'type': 'http.response.body',
-                        'body': json.dumps({"error": "n must be > 0"}).encode('utf-8'),
-                    })
+                    await send_resp(send, 400, json.dumps({"error": "n must be > 0"}).encode('utf-8'))
                     return
 
                 res = factorial(n)
-
-                await send({
-                    'type': 'http.response.start',
-                    'status': 200,
-                    'headers': [[b'content-type', b'application/json']],
-                })
-                await send({
-                    'type': 'http.response.body',
-                    'body': json.dumps({"result": res}).encode('utf-8'),
-                })
+                await send_resp(send, 200, json.dumps({"result": res}).encode('utf-8'))
             else:
-                await send({
-                    'type': 'http.response.start',
-                    'status': 422,
-                    'headers': [[b'content-type', b'application/json']],
-                })
-                await send({
-                    'type': 'http.response.body',
-                    'body': json.dumps({"error": "Not found n in params"}).encode('utf-8'),
-                })
+                await send_resp(send, 422, json.dumps({"error": "Not found n in params"}).encode('utf-8'))
 
             
         except:
-            await send({
-                'type': 'http.response.start',
-                'status': 422,
-                'headers': [[b'content-type', b'application/json']],
-            })
-            await send({
-                'type': 'http.response.body',
-                'body': json.dumps({"error": "Invalid parameter"}).encode('utf-8'),
-            })
-
+            await send_resp(send, 422, json.dumps({"error": "Invalid parameter"}).encode('utf-8'))
 
 
     elif scope.get("path") and "/mean" in scope.get("path"):
@@ -151,77 +104,27 @@ async def application(
             if body:
                 try:
                     int_numbs = json.loads(body)
-
                 except Exception:
-                    await send({
-                        'type': 'http.response.start',
-                        'status': 400,
-                        'headers': [[b'content-type', b'application/json']],
-                        })
-                    await send({
-                        'type': 'http.response.body',
-                        'body': json.dumps({"error": "params must be not empty"}).encode('utf-8'),
-                    })
+                    await send_resp(send, 400, json.dumps({"error": "params must be not empty"}).encode('utf-8'))
                     return
 
 
             if int_numbs == []:
-                await send({
-                    'type': 'http.response.start',
-                    'status': 400,  # BAD_REQUEST для пустого списка
-                    'headers': [[b'content-type', b'application/json']],
-                })
-                await send({
-                    'type': 'http.response.body',
-                    'body': json.dumps({"error": "params must be not empty"}).encode('utf-8'),
-                })
+                await send_resp(send, 400, json.dumps({"error": "params must be not empty"}).encode('utf-8'))
                 return
 
             if int_numbs is None:
-                await send({
-                    'type': 'http.response.start',
-                    'status': 422,  # UNPROCESSABLE_ENTITY для отсутствия параметров
-                    'headers': [[b'content-type', b'application/json']],
-                })
-                await send({
-                    'type': 'http.response.body',
-                    'body': json.dumps({"error": "params must be not empty"}).encode('utf-8'),
-                })
+                await send_resp(send, 422, json.dumps({"error": "params must be not empty list"}).encode('utf-8'))
                 return
                 
             average = sum(int_numbs) / len(int_numbs)
+            await send_resp(send, 200, json.dumps({"result": average}).encode('utf-8'))
 
-            if scope['type'] == 'http':
-                await send({
-                    'type': 'http.response.start',
-                    'status': 200,
-                    'headers': [[b'content-type', b'application/json']],
-                })
-                await send({
-                    'type': 'http.response.body',
-                    'body': json.dumps({"result": average}).encode('utf-8'),
-                })
         except:
-            await send({
-                'type': 'http.response.start',
-                'status': 422,
-                'headers': [[b'content-type', b'application/json']],
-            })
-            await send({
-                'type': 'http.response.body',
-                'body': json.dumps({"error": "Invalid parameter"}).encode('utf-8'),
-            })
+            await send_resp(send, 422, json.dumps({"error": "Invalid parameter"}).encode('utf-8'))
             
     else:
-        await send({
-            'type': 'http.response.start',
-            'status': 404,
-            'headers': [[b'content-type', b'application/json']],
-        })
-        await send({
-            'type': 'http.response.body',
-            'body': json.dumps({"error": "Not found"}).encode('utf-8'),
-        })
+        await send_resp(send, 404, json.dumps({"error": "Not found"}).encode('utf-8'))
 
     
 
