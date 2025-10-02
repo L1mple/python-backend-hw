b554290d8e1cacde1cb57658b78b6ece9ba4b5ea
Olga Ivleva <kiullikki@gmail.com>
2025-10-03 00:53:41 +0300

    HW2: Ивлева Ольга основное задание

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,3 +1,10 @@
 from fastapi import FastAPI
 
+from .api.cart import router as cart_router
+from .api.item import router as item_router
+
 app = FastAPI(title="Shop API")
+
+
+app.include_router(cart_router)
+app.include_router(item_router)
