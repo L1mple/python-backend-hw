e11ac111d8d1712a621e71fc9cd682da303c3d98
mrpetrakova2000 <mrpetrakova2000@gmail.com>
2025-10-02 12:02:42 +0300

    Shop API creating

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from hw2.hw.shop_api.store.routers import cart_router, item_router
+from shop_api.store.routers import cart_router, item_router
 
 app = FastAPI(title="Shop API")
 
