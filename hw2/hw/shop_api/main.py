afe759b8b33e25fbd168809b76284c28383dddb4
Alexey Laletin <alex.klark.laletin@yandex.com>
2025-10-05 13:24:04 +0000

    Implemented REST + RPC API for dummy market

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
+from shop_api.routers.item import router as item
+from shop_api.routers.cart import router as cart
 
 app = FastAPI(title="Shop API")
+
+app.include_router(item)
+app.include_router(cart)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, port=8001)
