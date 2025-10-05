983535be44c5263f356f9e11b804459d651b1dc2
Edgar Oganisian <edgar21082000@yandex.ru>
2025-10-06 00:27:22 +0300

    add hw2 + websockets_hw

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,3 +1,9 @@
 from fastapi import FastAPI
 
+from . import cart_routes, chat_routes, item_routes
+
 app = FastAPI(title="Shop API")
+
+app.include_router(item_routes.router)
+app.include_router(cart_routes.router)
+app.include_router(chat_routes.router)
