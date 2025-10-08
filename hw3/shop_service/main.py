31e5ffe1b5cbbdede4a4d36e3dd4743655286bf5
Alexander Zadorozhnyy <sasha.zadorojnii2013@yandex.ru>
2025-10-08 22:56:46 +0300

    Add prometheus instrumentator

--- hw3/shop_service/main.py
+++ hw3/shop_service/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from shop_service.routers.item import router as item_router
 from shop_service.routers.cart import router as cart_router
@@ -8,6 +9,8 @@ app = FastAPI(title="Shop API")
 
 app.include_router(item_router)
 app.include_router(cart_router)
+
+Instrumentator().instrument(app).expose(app)
 
 
 @app.get("/", include_in_schema=False)
