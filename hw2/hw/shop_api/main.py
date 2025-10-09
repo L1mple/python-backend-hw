0dd61c934a5f59a509939430412f0b0bee7cc403
Sergey Bubelov <bubelovv.dev@gmail.com>
2025-10-09 15:44:11 +0300

    feat: solved hw3

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, Query, HTTPException, Response
 from .routes.item import router as item_router
 from .routes.cart import router as cart_router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Shop API")
 
 app.include_router(item_router)
 app.include_router(cart_router)
+
+Instrumentator().instrument(app).expose(app)
