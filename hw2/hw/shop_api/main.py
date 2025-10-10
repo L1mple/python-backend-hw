257118b3867973dfb8ac5846e4ee368b01d02144
Said Kamalov <s.kamalovwork@yandex.ru>
2025-10-11 02:06:28 +0300

    complete hw3

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from .api.cart import router as cart_router
 from .api.item import router as item_router
@@ -6,3 +7,5 @@ from .api.item import router as item_router
 app = FastAPI(title="Shop API")
 app.include_router(cart_router)
 app.include_router(item_router)
+
+Instrumentator().instrument(app).expose(app)
