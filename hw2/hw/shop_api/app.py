a31e2b1ff4d2b965952d13ecc1468463220da728
Vilgiz <mr.igorvarlamov@mail.ru>
2025-10-11 20:36:33 +0300

    HW3 done

--- hw2/hw/shop_api/app.py
+++ hw2/hw/shop_api/app.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
-
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from .routers.items import router as items_router
 from .routers.carts import router as carts_router
 from .routers.chat import register_chat
 
-
 app = FastAPI(title="Shop API")
 
+instrumentator = Instrumentator()
+instrumentator.instrument(app)
+
+
+@app.on_event("startup")
+async def _startup_metrics():
+    instrumentator.expose(app, endpoint="/metrics", include_in_schema=False)
 
 app.include_router(items_router)
 app.include_router(carts_router)
-
 
 register_chat(app)
