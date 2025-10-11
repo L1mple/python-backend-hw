204bbc923c5098e23e8e802b8c67de607a18b355
glukhov324 <glukhovv.nn@gmail.com>
2025-10-11 14:01:42 +0700

    add hw3

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
 from shop_api.routers import cart_router, item_router, chat_router
 
 
+
 app = FastAPI(title="Shop API")
+Instrumentator().instrument(app).expose(app)
 app.include_router(cart_router)
 app.include_router(item_router)
 app.include_router(chat_router)
