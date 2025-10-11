cfb055ae9307727f82aea19e2467e7ab7f311a1f
Lina Veltman <veltman.lina@wb.ru>
2025-10-11 16:21:28 +0000

    hw3, added docker files, grafana, prometheus
    Signed-off-by: Lina Veltman <veltman.lina@wb.ru>

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
 from .routers import items, carts
 from .factory import ItemCreate
 from .database import db
 
 
 app = FastAPI(title="Shop API")
+
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(items.router)
 app.include_router(carts.router)
