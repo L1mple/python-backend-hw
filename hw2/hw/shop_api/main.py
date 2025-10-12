c6511c146c1b6a24bddee1c272f26cadb477f9b2
Блинова Мария <99964993+MuraBlinova@users.noreply.github.com>
2025-10-12 22:34:56 +0300

    HW3

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from .routes import router
+
 app = FastAPI(title="Shop API")
+Instrumentator().instrument(app).expose(app)
+
 app.include_router(router)
