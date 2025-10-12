bcec17caa0cf7337519608d46ca75c56861d9074
User Y1OV <sukolov-21@edu.ranepa.ru>
2025-10-12 21:37:06 +0300

    hw3

--- lecture3/demo_service/api.py
+++ lecture3/demo_service/api.py
@@ -1,42 +1,10 @@
-from http import HTTPStatus
-from typing import Annotated
-import random
-
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
 
-from demo_service import store
-from demo_service.contracts import UserRequest, UserResource
+from shop_api.routers import cart_router, item_router, chat_router
 
-app = FastAPI(title="Demo User API")
+app = FastAPI(title="Shop API")
 Instrumentator().instrument(app).expose(app)
-
-
-def maybe_raise_random_error():
-    if random.random() < 0.1:
-        raise HTTPException(
-            status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            detail="Random error occurred"
-        )
-
-
-@app.post(
-    "/create-user",
-    response_model=UserResource,
-    status_code=HTTPStatus.CREATED,
-)
-async def create_user(body: UserRequest) -> UserResource:
-    maybe_raise_random_error()
-    return store.insert(body)
-
-
-@app.post("/get-user")
-async def get_user(id: Annotated[int, Query()]) -> UserResource:
-    maybe_raise_random_error()
-
-    resource = store.select(id)
-
-    if not resource:
-        raise HTTPException(HTTPStatus.NOT_FOUND)
-
-    return resource
+app.include_router(cart_router)
+app.include_router(item_router)
+app.include_router(chat_router)
