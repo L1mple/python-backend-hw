042e0987f046d8baf3b526e7570ffc3b5ec64b2e
Alexey Laletin <alex.klark.laletin@yandex.com>
2025-10-12 18:11:16 +0000

    Promehteus + Grafana monitoring for HW2

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from shop_api.routers.item import router as item
 from shop_api.routers.cart import router as cart
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Shop API")
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(item)
 app.include_router(cart)
