f9e15802383b308c23d2cd80a57f51824a7fb424
Александра Цепкова <tsepkova.ai@phystech.edu>
2025-10-12 17:28:22 +0200

    Add monitoring

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
 from shop_api.routes import router
 
 app = FastAPI(title="Shop API")
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(router)
