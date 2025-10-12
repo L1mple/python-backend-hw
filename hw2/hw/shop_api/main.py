1a16b709fb312e205f51b5e8ae4185eb85a98d3d
SirDratuti <belitsckiy@mail.ru>
2025-10-12 16:18:51 +0300

    graphana and prometheus

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-
+from prometheus_fastapi_instrumentator import Instrumentator
 from hw2.hw.shop_api.routes import cartRouter, itemRouter
 
 app = FastAPI(title="Shop API")
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(cartRouter)
 app.include_router(itemRouter)
