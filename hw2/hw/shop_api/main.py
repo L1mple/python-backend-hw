db90bff20a8deeae88427f3a9f8652b599416351
sssay10 <duvakin.k@gmail.com>
2025-10-28 00:37:12 +0300

    Added docker, grafana and prometheus(HW 3)

--- hw2/hw/shop_api/main.py
+++ hw2/hw/shop_api/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from .routes import items_router, carts_router
 
 app = FastAPI(title="Shop API")
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(items_router)
 app.include_router(carts_router)
+
+
